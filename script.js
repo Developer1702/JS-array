11ce691e2ded34cbe509e5efdecce661b0ac0139
Muhammadqodir <usmonovmuhammadqodir79@gmail.com>
2023-07-20 11:58:33 +0300

    site

---
+++ script.js
@@ -0,0 +1,11 @@
+let str ="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, mollitia?";
+let array = str.split("");
+
+let box = 0;
+for (let i =0; i < array.length; i++) {
+  if (array[i]== "e" || array[i] == "i" ||array[i] == "u" || array[i] == "o" || array[i] == "a") {
+    box++
+  }
+}
+
+console.log(box);
